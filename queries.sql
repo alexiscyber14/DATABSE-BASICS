5c65c52f0b18e8adc0d601eed6eacfdf5216be65
alexiscyber14 <142sledgehammer14@gmail.com>
2023-05-08 12:08:46 +0100

    adjust queries

--- queries.sql
+++ queries.sql
@@ -41,6 +41,7 @@ SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
 
+
 BEGIN;
 -- Delete all animals born after Jan 1st, 2022
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
@@ -54,3 +55,26 @@ ROLLBACK TO update_weights;
 UPDATE animals SET weight_kg = -1 * weight_kg WHERE weight_kg < 0;
 -- Commit the transaction
 COMMIT;
+
+SELECT * FROM animals;
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, AVG(escape_attempts) as avg_escape_attempts 
+FROM animals 
+GROUP BY neutered 
+ORDER BY AVG(escape_attempts) DESC LIMIT 1;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg) 
+FROM animals 
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts) 
+FROM animals 
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' 
+GROUP BY species;
+
--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE animals
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    name text COLLATE pg_catalog."default" NOT NULL,
+    name text  NOT NULL,
     date_of_birth date,
     escape_attempts integer,
     neutered boolean,
     weight_kg numeric,
-    species text COLLATE pg_catalog."default",
+    species text,
     CONSTRAINT animals_pkey PRIMARY KEY (id)
 )
